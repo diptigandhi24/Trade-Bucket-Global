fc9c7c101efb4cb68a67fd5867db3bbcaaaedc6e
Dipti Gandhi <gandhidipti.s@gmail.com>
2016-01-25 12:35:12 +0530

    Displaying the menu and submenu of the mobile navigation with the toggling function.

--- wp-content/themes/seo/js/navigation.js
+++ wp-content/themes/seo/js/navigation.js
@@ -5,15 +5,16 @@
  * support for dropdown menus.
  */
 ( function() {
-	var container, button, menu, links, subMenus, menuicon , shownav;
+	var container, button, menu, links, subMenus, menuicon , shownav,
+	servicesmenu, serviceButton;
 
 	container = document.getElementsByClassName( 'cont-nav' )[0];
 	if ( ! container ) {
 		return;
 	}
 	else{console.log("yes container object exits" , container.childNodes.length);
-	for (var i = 1 ; i < container.childNodes.length ; i++) {
-		console.log("value of i ",i , container.childNodes[i]);
+	for (var i = 0 ; i < container.childNodes.length ; i++) {
+		console.log("container Element Object  : ",i , container.childNodes[i]);
 	};
 
 }
@@ -22,23 +23,15 @@
 
 
 	menu = container.getElementsByTagName( 'ul' )[0];
-	if ( ! menu ) {
-		return;
-	}
-	else{console.log("yes button object exits" , menu.childNodes.length);
-		for (var i = 0 ; i <= menu.childNodes.length ; i++) {
-		console.log("value of i ",i , menu.childNodes[i]);
-	};}
+	
 	// Hide menu toggle button if menu is empty and return early.
 	if ( 'undefined' === typeof menu ) {
 		button.style.display = 'none';
 		return;
 	}
-
-	menu.setAttribute( 'aria-expanded', 'false' );
-	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
-		menu.className += ' nav-menu';
-	}
+	servicesmenu=container.getElementsByClassName('sub-menu')[0];
+	serviceButton=document.getElementById('serviceExpand');
+	
 	shownav=container.getElementsByClassName('test')[0];
 	button.onclick = function() {
 		
@@ -58,43 +51,23 @@
 			shownav.style.display="block";
 		}
 	};
-
-	// Get all the link elements within the menu.
-	links    = menu.getElementsByTagName( 'a' );
-	subMenus = menu.getElementsByTagName( 'ul' );
-
-	// Set menu items with submenus to aria-haspopup="true".
-	for ( var i = 0, len = subMenus.length; i < len; i++ ) {
-		subMenus[i].parentNode.setAttribute( 'aria-haspopup', 'true' );
-		console.log("link ",i , links[i]);
-	}
-
-	// Each time a menu link is focused or blurred, toggle focus.
-	for ( i = 0, len = links.length; i < len; i++ ) {
-		links[i].addEventListener( 'focus', toggleFocus, true );
-		links[i].addEventListener( 'blur', toggleFocus, true );
-	}
-
-	/**
-	 * Sets or removes .focus class on an element.
-	 */
-	function toggleFocus() {
-		var self = this;
-		console.log("link is not focus");
-		// Move up through the ancestors of the current link until we hit .nav-menu.
-		while ( -1 === self.className.indexOf( 'nav-menu' ) ) {
-
-			// On li elements toggle the class .focus.
-			if ( 'li' === self.tagName.toLowerCase() ) {
-				if ( -1 !== self.className.indexOf( 'focus' ) ) {
-					self.className = self.className.replace( ' focus', '' );
-
-				} else {
-					self.className += ' focus';
-				}
-			}
-
-			self = self.parentElement;
+	serviceButton.onclick = function() {
+		
+		if ( -1 !== servicesmenu.className.indexOf( 'doggled' ) ) {
+			console.log("inside if condition ");
+			serviceButton.innerHTML = "#menu-icon";
+			servicesmenu.className = servicesmenu.className.replace( ' doggled', '' );
+			serviceButton.setAttribute( 'aria-expanded', 'false' );
+			menu.setAttribute( 'aria-expanded', 'false' );
+			servicesmenu.style.display="none";
+		} else {
+			serviceButton.innerHTML = "X";
+			console.log("inside onclick else condition" ,servicesmenu.className.indexOf( 'doggled' ));
+			servicesmenu.className += ' doggled';
+			serviceButton.setAttribute( 'aria-expanded', 'true' );
+			menu.setAttribute( 'aria-expanded', 'true' );
+			servicesmenu.style.display="block";
 		}
-	}
+	};
+	
 } )();
