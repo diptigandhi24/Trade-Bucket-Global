dba0fe2884a85d07b2865508db50569419082baf
Dipti Gandhi <gandhidipti.s@gmail.com>
2016-02-04 15:18:22 +0530

    Applying slideshow to the site

--- wp-content/themes/seo/home.php
+++ wp-content/themes/seo/home.php
@@ -20,17 +20,22 @@
 
 			<?php if ( get_theme_mod( 'seo_canonical' ) ) : ?>
 			<link rel="canonical" href="<?php echo esc_url( get_theme_mod( 'seo_canonical' ) ); ?>">
-			<?php endif; ?>  				
+			<?php endif; ?>  	
+			<script src="http://ivyhome.com/wordpressTradeGlobal/wp-includes/js/jquery/jquery-1.7.min.js" 
+				type="text/javascript"></script>
+			<script src="http://ivyhome.com/wordpressTradeGlobal/wp-content/themes/seo/js/jquery/featureCarousel.min.js'" 
+				type="text/javascript"></script>
+			
+				
  			
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'seo' ); ?></a>
 	<div class="dotted">
-		<header id="masthead" class="site-header" style="background: url('<?php header_image(); ?>') repeat; height:10%">	     
-			<div class="site-branding">
+		<header id="masthead" class="site-header box-border" style="height:170px ">	     
+			<div class="site-branding ">
 				<img src="<?php bloginfo('stylesheet_directory'); ?>/img/Trade-Bucket-Global.png" class="logoImage"/>
 				<img class="calllogo" src="<?php bloginfo('stylesheet_directory'); ?>/img/resizecalllogo.png"/>
 				<p class="calldetails" >call us: +91-98-2095-5392</p>
@@ -48,7 +53,7 @@
 						</a>
 						<div class="test">
 						 <ul id="menu-menu-1" class="menu"><li id="menu-item-76" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-76">
-<a href="<?php bloginfo('url'); ?>">Home</a>
+<a href="<?php bloginfo('url'); ?>" >Home</a>
 	</li>
 <li id="menu-item-78" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-78">
 <a href="<?php bloginfo('url'); ?>/index.php/contact-us/">Contact Us</a>
@@ -101,10 +106,10 @@
 			</div>
 
 		</header><!-- #masthead -->
-	</div>			
-		<div class="seos-slide" >
-			
-		</div>
+	</div>	
+	<div style="padding-top:10px"><?php wd_slider(4); ?>/>
+	<div class="slidetext"> <p style="margin:auto ; padding:auto; width:700px">Safely Product Delivery WorldWide</p></div>></div>		
+		
 		<div class="featured-box">   
 			<div class="wrapper">
 				<div class="home-box">
